cc1dc78140bc1b7b4252dd5a85c17f8d45defe2a
AlirieGray <alirie.gray@gmail.com>
2024-01-15 22:31:45 -0800

    add contact info

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Contact.css'
 
 function Contact() {
   return (
     <div>
-        <p>Email us at info@twiceasbright.games </p>
+        <p>Email us at <a className="contact" href="mailto: info@twiceasbright.games">info@twiceasbright.games</a></p>
 
-        <p>Follow us on Twitter at @2xbrightgames</p>
+        <p>Follow us on Twitter at <a className="contact" href="https://twitter.com/2xbrightgames">@2xbrightgames</a></p>
+
+        <p>You can view the script of the game <a className="contact" href="https://github.com/AlirieGray/prologue">here.</a></p>
     </div>
 
   );
